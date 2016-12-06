5d229559f059cc570788ff8b01f55c288afc42c1
Max Bondarchuk <bondarchuk.m.y@gmail.com>
2016-12-06 12:36:28 +0200

    Revert "Lab2 release"
    This reverts commit c8f6d6cf3b1c7a75a2ba5991c5302723807c4f63.

--- Lab1/settings.py
+++ Lab1/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,7 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 ROOT_URLCONF = 'Lab1.urls'
